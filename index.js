99b1c5321f8fbe5b5a94ef045ff40cfbb22a91ec
Shreya <shreya11072003@gmail.com>
2022-05-03 15:54:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const port =process.env.PORT|| 2000;
+const router = express.Router();
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/main.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+  app.use('/', router);
+  app.listen(port);
+
+console.log(`Running at port ${port}`);
+  
